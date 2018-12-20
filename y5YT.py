d0d3365d7fab6700f9a7f136562d1d9cb145f429
Tadashi Fujinaga <tadashifujinaga@gmail.com>
2018-12-20 13:07:45 +0900

    Add files via upload
    オープン講座 sample01-04追加

---
+++ y5YT.py
@@ -0,0 +1,18 @@
+import random
+
+print('じゃんけんスタート')
+
+print('あなたの手を入力してください')
+my_hand = int(input('0:グー, 1:チョキ, 2:パー'))
+you_hand = random.randint(0, 2)
+
+if my_hand == 0:
+  if you_hand == 0:
+    print('あいこ')
+  elif you_hand == 1:
+    print('勝ち')
+elif my_hand == 1:
+  if you_hand == 0:
+    print('負け')
+  elif you_hand == 1:
+    print('あいこ')
